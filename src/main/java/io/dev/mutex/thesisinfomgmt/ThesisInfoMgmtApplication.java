35d1d8323d5b4552034d735ef50e7dcb010145b6
Christian Morales <christiancarven.morales@gmail.com>
2023-08-26 14:52:34 +0800

    TM-1: Setup backend with DB connection

---
+++ src/main/java/io/dev/mutex/thesisinfomgmt/ThesisInfoMgmtApplication.java
@@ -0,0 +1,12 @@
+package io.dev.mutex.thesisinfomgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThesisInfoMgmtApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ThesisInfoMgmtApplication.class, args);
+  }
+}
